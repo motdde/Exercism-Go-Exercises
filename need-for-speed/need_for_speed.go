492648d41104385b78428e5ed6d312a90acddebd
Oluwaseun <motdde@users.noreply.github.com>
2022-01-10 18:36:03 +0100

    need for speed

---
+++ need-for-speed/need_for_speed.go
@@ -0,0 +1,42 @@
+package speed
+
+type Car struct {
+	battery      int
+	batteryDrain int
+	speed        int
+	distance     int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	var distance int
+	battery := 100
+	car := Car{speed: speed, batteryDrain: batteryDrain, battery: battery, distance: distance}
+	return car
+}
+
+// TODO: define the 'Track' type struct
+type Track struct {
+	distance int
+}
+
+// NewTrack created a new track
+func NewTrack(distance int) Track {
+	return Track{distance: distance}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive on more time,
+// the car will not move.
+func Drive(car Car) Car {
+	if (car.battery / car.batteryDrain) >= 1 {
+		car.battery -= car.batteryDrain
+		car.distance += car.speed
+	}
+	return car
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	carDistance := car.battery / car.batteryDrain * car.speed
+	return carDistance >= track.distance
+}
