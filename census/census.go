055f52d796d628478edd8dd32563d959a1e7c7c3
Oluwaseun <motdde@users.noreply.github.com>
2022-01-21 14:05:15 +0100

    Completed Census

---
+++ census/census.go
@@ -0,0 +1,35 @@
+// Package census simulates a system used to collect census data.
+package census
+
+// Resident represents a resident in this city.
+type Resident struct {
+	Name    string
+	Age     int
+	Address map[string]string
+}
+
+// NewResident registers a new resident in this city.
+func NewResident(name string, age int, address map[string]string) *Resident {
+	return &Resident{name, age, address}
+}
+
+// HasRequiredInfo determines if a given resident has all of the required information.
+func (r *Resident) HasRequiredInfo() bool {
+	return len(r.Name) != 0 && (len(r.Address["street"]) != 0)
+}
+
+// Delete deletes a resident's information.
+func (r *Resident) Delete() {
+	*r = Resident{}
+}
+
+// Count counts all residents that have provided the required information.
+func Count(residents []*Resident) int {
+	count := 0
+	for _, resident := range residents {
+		if resident.HasRequiredInfo() {
+			count++
+		}
+	}
+	return count
+}
